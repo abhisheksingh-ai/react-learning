5449cf45a4c5cb1184c5053a3cb8538b6846cd1e
Abhishek Singh <asingh19082000@gmail.com>
2025-03-07 19:40:55 +0530

    Episode-1

---
+++ app.js
@@ -0,0 +1,15 @@
+const parent = React.createElement(
+    "div",
+    {id: "parent"},[
+        React.createElement("div", { id: "child1" }, [
+            React.createElement("h1", { id: "child1a" }, "I am h1"),
+            React.createElement("h2", { id: "child1a" }, "I am h2")
+        ]),
+        React.createElement("div", { id: "child2" }, [
+            React.createElement("h1", { id: "child1b" }, "I am h1"),
+            React.createElement("h2", { id: "child1b" }, "I am h2")
+        ])
+    ]
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
