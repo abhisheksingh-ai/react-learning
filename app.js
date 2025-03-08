9525b608fa5e4ccbda1134a5427874f0042d1352
Abhishek Singh <asingh19082000@gmail.com>
2025-03-08 18:49:21 +0530

    Episode:3 jsx syntax, Babel Learning

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement(
-    "div",
-    {id: "parent"},[
-        React.createElement("div", { id: "child1" }, [
-            React.createElement("h1", { id: "child1a" }, "I am h1"),
-            React.createElement("h2", { id: "child1a" }, "I am h2")
-        ]),
-        React.createElement("div", { id: "child2" }, [
-            React.createElement("h1", { id: "child1b" }, "I am h1"),
-            React.createElement("h2", { id: "child1b" }, "I am h2")
-        ])
-    ]
-);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+const jsxHeading = <h1>Namastey React from JSX🚀</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
