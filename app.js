c587a3dac96d24824bd3e78cb4b2eed651eb4ee5
Abhishek Singh <asingh19082000@gmail.com>
2025-03-13 08:33:11 +0530

    Food Order app, built Header compnent html

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-//functional component
-const Title = () => (
-    <h1>Functional Component in React🚀</h1>
-);
+//functional component is a function that return jsx
 
-const Heading = () => (
-    <>
-        <Title />
-        <h2>Functional component is just javascript functiona that return jsx</h2>
-    </>
-)
+const Header = () => {
+    return (
+        <div className="Header">
+            <div className="logo">
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart Us</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading />);
+root.render(<AppLayout />);
